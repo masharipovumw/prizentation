e9641bf40421434059b19f5873fb97dd03cd98da
unknown <umrbekmasharopov23@gmail.com>
2022-11-22 16:34:31 +0500

    commit

---
+++ js/swiper.js
@@ -0,0 +1,217 @@
+const swiper = new Swiper('.slider', {
+    pagination: {
+        el: '.swiper-pagination',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+})
+
+
+const verniy = document.querySelector('.verniy')
+const verniy1 = document.querySelector('.verniy1')
+const verniy2 = document.querySelector('.verniy2')
+const verniy3 = document.querySelector('.verniy3')
+const verniy4 = document.querySelector('.verniy4')
+const verniy5 = document.querySelector('.verniy5')
+const verniy6 = document.querySelector('.verniy6')
+const verniy7 = document.querySelector('.verniy7')
+const verniy8 = document.querySelector('.verniy8')
+const verniy9 = document.querySelector('.verniy9')
+
+const neverniy = document.querySelector('.neverniy')
+const neverniy1 = document.querySelector('.neverniy1')
+const neverniy2 = document.querySelector('.neverniy2')
+const neverniy3 = document.querySelector('.neverniy3')
+const neverniy4 = document.querySelector('.neverniy4')
+const neverniy5 = document.querySelector('.neverniy5')
+const neverniy6 = document.querySelector('.neverniy6')
+const neverniy7 = document.querySelector('.neverniy7')
+const neverniy8 = document.querySelector('.neverniy8')
+const neverniy9 = document.querySelector('.neverniy9')
+const neverniy10 = document.querySelector('.neverniy10')
+const neverniy11 = document.querySelector('.neverniy11')
+const neverniy12 = document.querySelector('.neverniy12')
+const neverniy13 = document.querySelector('.neverniy13')
+const neverniy14 = document.querySelector('.neverniy14')
+const neverniy15 = document.querySelector('.neverniy15')
+const neverniy16 = document.querySelector('.neverniy16')
+const neverniy17 = document.querySelector('.neverniy17')
+const neverniy18 = document.querySelector('.neverniy18')
+const neverniy19 = document.querySelector('.neverniy19')
+
+
+
+const modal = document.querySelector('.modal')
+const modalX = document.querySelector('.modalx')
+const x = document.querySelector('.X')
+const X = document.querySelector('.x')
+
+verniy.addEventListener('click', function(){
+  modal.classList.add('active')
+})
+
+verniy1.addEventListener('click', function(){
+  modal.classList.add('active')
+})
+
+verniy2.addEventListener('click', function(){
+  modal.classList.add('active')
+})
+
+verniy3.addEventListener('click', function(){
+  modal.classList.add('active')
+})
+
+verniy4.addEventListener('click', function(){
+  modal.classList.add('active')
+})
+
+verniy5.addEventListener('click', function(){
+  modal.classList.add('active')
+})
+
+verniy6.addEventListener('click', function(){
+  modal.classList.add('active')
+})
+
+verniy7.addEventListener('click', function(){
+  modal.classList.add('active')
+})
+
+verniy8.addEventListener('click', function(){
+  modal.classList.add('active')
+})
+
+verniy9.addEventListener('click', function(){
+  modal.classList.add('active')
+})
+
+
+
+
+
+neverniy.addEventListener('click', function(){
+  modalX.classList.add('active')
+})
+
+neverniy1.addEventListener('click', function(){
+  modalX.classList.add('active')
+})
+
+neverniy2.addEventListener('click', function(){
+  modalX.classList.add('active')
+})
+
+neverniy3.addEventListener('click', function(){
+  modalX.classList.add('active')
+})
+
+neverniy4.addEventListener('click', function(){
+  modalX.classList.add('active')
+})
+
+neverniy5.addEventListener('click', function(){
+  modalX.classList.add('active')
+})
+
+neverniy6.addEventListener('click', function(){
+  modalX.classList.add('active')
+})
+
+neverniy7.addEventListener('click', function(){
+  modalX.classList.add('active')
+})
+
+neverniy8.addEventListener('click', function(){
+  modalX.classList.add('active')
+})
+
+neverniy9.addEventListener('click', function(){
+  modalX.classList.add('active')
+})
+
+neverniy10.addEventListener('click', function(){
+  modalX.classList.add('active')
+})
+
+neverniy11.addEventListener('click', function(){
+  modalX.classList.add('active')
+})
+
+neverniy12.addEventListener('click', function(){
+  modalX.classList.add('active')
+})
+
+neverniy13.addEventListener('click', function(){
+  modalX.classList.add('active')
+})
+
+neverniy14.addEventListener('click', function(){
+  modalX.classList.add('active')
+})
+
+neverniy15.addEventListener('click', function(){
+  modalX.classList.add('active')
+})
+
+neverniy16.addEventListener('click', function(){
+  modalX.classList.add('active')
+})
+
+neverniy17.addEventListener('click', function(){
+  modalX.classList.add('active')
+})
+
+neverniy18.addEventListener('click', function(){
+  modalX.classList.add('active')
+})
+
+neverniy19.addEventListener('click', function(){
+  modalX.classList.add('active')
+})
+
+
+
+
+x.addEventListener('click', function(){
+  modal.classList.remove('active')
+})
+
+X.addEventListener('click', function(){
+  modalX.classList.remove('active')
+})
+
+modal.addEventListener('click', function(){
+  modal.classList.remove('active')
+})
+
+modalX.addEventListener('click', function(){
+  modalX.classList.remove('active')
+})
+
+
+
+// const button = document.querySelectorAll('input');
+// const top = document.querySelector('.modal')
+
+
+// for(const item of button){
+//     item.addEventListener('click', function(){
+//         let top =item.nextElementSibling;
+//         if(top.classList.contains('active')){
+//             top.classList.remove('active')
+//         }else{
+//             for(const el of button){
+//                 if(el.nextElementSibling.classList.contains('active')){
+//                     el.nextElementSibling.classList.remove('active')
+//                 }
+//             }
+//             top.classList.add('active')
+//         }
+        
+//     })
+// }
+
+
